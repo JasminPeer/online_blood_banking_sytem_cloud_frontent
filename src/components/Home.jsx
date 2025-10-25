0dc09d0588341c68892bc08a07995729d16e7948
Jasmin fathima <jasminpeer006@gmail.com>
2025-10-25 07:30:02 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import bloodDonationImage from "../image/bllod.jpg";
 import {
-  AppBar,
-  Toolbar,
   Typography,
   Container,
   Grid,
